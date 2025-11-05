c6c09204a109fbb5683fe7298ec34f3f24e6c090
Grant Herman <grantlouisherman041@gmail.com>
2025-11-04 21:17:29 -0500

    start of chapter 2

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -1,5 +1,44 @@
-#include <libsdb/libsdb.hpp>
+#include <iostream>
+#include <unistd.h>
 
-int main() {
-    sdb::say_hello();
+namespace {
+    pid_t attach(int argc, const char** argv) {
+        pid_t pid = 0;
+        if(argc == 3 && argv[1] == std::string_view("-p")){
+            pid = std::atoi(argv[2]);
+            if(pid <= 0){
+                std::cerr << "Invalid PID\n";
+                return -1;
+            }
+            if(ptrace(PTRACE_ATTACH, pid, /*addr=*/ nullptr, /*data=*/ nullptr) < 0){
+                std::perror("Could Not Attach");
+                return -1;
+            }
+        } else {
+            const char* program_path = argv[1];
+            if((pid=fork()) < 0){
+                std::perror("Fork failed");
+            }
+            if(pid == 0){
+                if(ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
+                    std::perror("Tracing Failed");
+                    return -1;
+                }
+                if(execlp(program_path, program_path, nullptr) < 0){
+                    std::perror("Exec Failed");
+
+                    return -1;
+                }
+            }
+
+        }
+        return pid;
+    }
+}
+int main(int argc, const char** argv) {
+    if(argc == 1){
+        std::cerr << "No arguments\n";
+        return -1;
+    }
+    pid_t pid = attach(argc, argv);
 }
